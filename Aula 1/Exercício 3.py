81bf402d976bd98762e415f376665c1f6f5659b2
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-16 16:49:46 -0300

    Update Exercício 3.py

--- Aula 1/Exercício 3.py
+++ Aula 1/Exercício 3.py
@@ -9,9 +9,9 @@
 
 altura = int(input("Digite a altura da pirâmide: "))
 
-while (altura <=0):
+while (altura <=0): #esse while serve para que o usuário não de um tamanho menor ou igual a zero.
     print("A altura da pirâmide deve ser maior do que zero")
-    altura = int(input("Digite um valor diferente para a altura: "))
+    altura = int(input("Digite um valor diferente para a altura: ")) 
 
 for i in range(altura):
     for j in range (altura - (i + 1)): #número de espaços necessários até a #
