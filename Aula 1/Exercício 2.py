f38eb161200658a732ed1cf0cdb9eb9b923a0e79
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-16 10:27:36 -0300

    Update Exercício 2.py

--- Aula 1/Exercício 2.py
+++ Aula 1/Exercício 2.py
@@ -2,18 +2,10 @@
 # e cada aluno é representado por um dicionário com informações sobre seu nome e idade.
 
 #a) Criar as turmas: Crie duas turmas, cada uma contendo uma lista de dicionários representando os alunos.
-#Cada dicionário deve ter chaves "nome" e "idade";
-
-#b) Adicionar um novo aluno: Adicione um novo aluno em uma das turmas.
-
-#c) Remover um aluno: Remova um aluno de uma das turmas.
-
-#d) Exibir os alunos de uma turma.
-
-#a)
-turmas = {
-    "Turma A": [
-        {"nome": "aluno1", "idade": 20, "id": 1},
+#Cada dicionário deve ter chaves "nome" e "idade".
+turmas = { #criamos um dicionário "turmas" que contém a "Turma A" e a "Turma B".
+    "Turma A": [ #Cada uma das turmas é uma lista que contém os alunos e suas informações no formato de dicionário.
+        {"nome": "aluno1", "idade": 20, "id": 1}, #por mais que o exercício não peça um id, podemos considerar esse id como uma matrícula, facilitando achar os alunos.
         {"nome": "aluno2", "idade": 20, "id": 2}
     ],
 
@@ -23,27 +15,24 @@ turmas = {
     ]
 }
 
-#b)
+#b) Adicionar um novo aluno: Adicione um novo aluno em uma das turmas.
 NovoAluno = {"nome": "aluno5", "idade": 23, "id": 5}
 
-turmas["Turma A"].append(NovoAluno)
+turmas["Turma A"].append(NovoAluno) #por turma ser uma lista com dicionários, podemos usar o .append
 
-#c)
-id: int = 3
+#c) Remover um aluno: Remova um aluno de uma das turmas.
+id: int = 3 #para não removermos dois alunos com nomes iguais, utilizamos um id único para cada um.
 
+for turma in turmas.keys(): #com esse for passamos pelas turmas A e B
+    for aluno in turmas[turma]: #com esse for passamos pelos alunos em cada turma
+        if aluno["id"] == id: #utilizamos o if para achar o aluno com mesmo id proposto no início, nesse caso o id 3.
+            turmas[turma].remove(aluno) #removemos o aluno com o id 3.
+
+#d) Exibir os alunos de uma turma.
 for turma in turmas.keys():
-    for aluno in turmas[turma]:
-        if aluno["id"] == id:
-            turmas[turma].remove(aluno)
+    print(turma) #retorna Turma A e depois Turma B
 
-
-#d)
-for turma in turmas.keys():
-    print(turma)
-
-    print("Alunos: ")
-
-    for aluno in turmas[turma]:
+    for aluno in turmas[turma]: #retorna os alunos dentro de cada turma.
         print(f"Nome: {aluno["nome"]}, Idade: {aluno["idade"]} ")
     
-    print()
+    print() #quebra de linha para cada aluno
