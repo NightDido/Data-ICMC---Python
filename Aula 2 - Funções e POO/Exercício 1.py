761ac568da02ae576dea6f28f1913ab6913692e3
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-16 23:38:26 -0300

    Update Exercício 1.py

--- Aula 2 - Funções e POO/Exercício 1.py
+++ Aula 2 - Funções e POO/Exercício 1.py
@@ -1,20 +1,20 @@
 #Exercício 1: Faça uma função que dado um vetor de inteiros retorne o maior inteiro do vertor.
 
-def MaiorValor(vetor):
-    if (len(vetor) ==0):
-        return None
+def MaiorValor(vetor): #para aprender a usar "def", fizemos uma função que retorna o maior valor dentro do vetor. 
+    if (len(vetor) ==0): #verificar que o vetor não é vazio.
+        return None 
     
-    maior = vetor[0]
+    maior = vetor[0] #pegamos o primeiro objeto do vetor e armazenamos.
 
-    for i in range (1, len(vetor)):
+    for i in range (1, len(vetor)): #começamos no índice 1, pois temos o número que está no índice 0 armazenado na variável "maior".
         if vetor[i] > maior:
             maior = vetor[i]
     
-    return maior
+    return maior #retorna o maior número do vetor.
                     
         
-vetor = [1, 2, 29, 7]
+vetor = [1, 2, 29, 7] #vetor de teste.
 
-print(MaiorValor(vetor))
+print(MaiorValor(vetor)) #retorna 29.
 
-print(max(vetor))
+print(max(vetor)) #como existe o "max", não é necessário criar uma função. Também retorna 29.
