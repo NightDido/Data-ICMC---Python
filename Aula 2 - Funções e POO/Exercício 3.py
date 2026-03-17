604c2eac400af371b4b2c116d23bcc1ab7e480e8
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-17 10:55:31 -0300

    Update Exercício 3.py

--- Aula 2 - Funções e POO/Exercício 3.py
+++ Aula 2 - Funções e POO/Exercício 3.py
@@ -1,10 +1,10 @@
 #Crie uma função que dado dois vetores, 
 #retorne um novo vetor que é a concatenação de ambos os vetores
 
-def Concatenar (vetor1, vetor2):
-    NovoVetor = vetor1.copy()
+def Concatenar (vetor1, vetor2): #Une um vetor no outro.
+    NovoVetor = vetor1.copy() #Se não usarmos o .copy, a função afetará o vetor1.
 
-    for elemento in vetor2:
+    for elemento in vetor2: 
         NovoVetor.append(elemento)
     
     return NovoVetor
@@ -14,5 +14,5 @@ vetor2 = [4,5,6]
 
 NovoVetor = Concatenar(vetor1, vetor2)
 
-print(NovoVetor)
-print(vetor1)
+print(NovoVetor) #Retornará [1,2,3,4,5,6].
+print(vetor1) #Retornará [1,2,3], pois usamos copy anteriormente.
