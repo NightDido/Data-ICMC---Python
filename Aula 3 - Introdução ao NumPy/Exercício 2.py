d1fb007af41f2698cc3fe23ce12f74ce18f5f981
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-21 19:27:40 -0300

    Update Exercício 2.py

--- Aula 3 - Introdução ao NumPy/Exercício 2.py
+++ Aula 3 - Introdução ao NumPy/Exercício 2.py
@@ -3,8 +3,8 @@
 
 import numpy as np
 
-original = np.array([2, 5, 7, 9, 1, 4, 5])
-def remove_primeiro_último(arr):
-    return arr[1:-1]
+original = np.array([2, 5, 7, 9, 1, 4, 5]) #definimos uma matriz original
+def remove_primeiro_último(arr): #como o exercício manda criamos uma função, precisamos usar o "def"
+    return arr[1:-1] #retorna o array sem o primeiro elemento (indíce 0), então começa no segundo e vai até o último elemento sem ele estar incluso, podemos usar o "-1" sempre que quiseremos chamar o último elemento.
 
-print(remove_primeiro_último(original))
+print(remove_primeiro_último(original)) #retorna [5 7 9 1 4]
