d1e44e282c330e1ce67722beef3e206edc350945
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-21 19:35:36 -0300

    Update Exercício 3.py

--- Aula 3 - Introdução ao NumPy/Exercício 3.py
+++ Aula 3 - Introdução ao NumPy/Exercício 3.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
-arr = np.arange(101)
-pares_bool = (arr % 2 == 0)
-print(arr[pares_bool].sum())
+arr = np.arange(101) #crie um array de 0 a 100.
+pares_bool = (arr % 2 == 0) #retorna uma matriz de "True" e "False", onde "True" quer dizer que o número é par.
+print(arr[pares_bool].sum())#"arr[pares_bool]" troca onde é "True" pelo seus correspondentes da lista original e o ".sum()" soma todos os elementos do array.
+#resultado: 2550
