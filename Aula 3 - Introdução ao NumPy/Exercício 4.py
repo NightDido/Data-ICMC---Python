db58ff301ef69d200ecb68613dcb2dfb28b9dd64
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-22 01:04:24 -0300

    Update Exercício 4.py

--- Aula 3 - Introdução ao NumPy/Exercício 4.py
+++ Aula 3 - Introdução ao NumPy/Exercício 4.py
@@ -5,9 +5,9 @@
 
 import numpy as np
 
-arr = np.arange(101)
-impares_bool = (arr % 2 != 0)
-impares = arr[impares_bool]
-div_3_5 = (impares % 3 == 0) & (impares % 5 == 0)
-print(impares[div_3_5].cumsum())
+arr = np.arange(101) #Gera um array de 0 a 100
+impares_bool = (arr % 2 != 0) #Filtra apenas os ímpares, criando "True" caso sejam impares e "False" caso contrário.
+impares = arr[impares_bool] #Altera os valores "True" para valores do array com o mesmo índice.
+div_3_5 = (impares % 3 == 0) & (impares % 5 == 0) #Filtra os valores impares que são divisiveis por 3 e 5, nesse caso criando "True" e caso contrário "False"
+print(impares[div_3_5].cumsum()) #"Impares[div_3_5]" gera um array de [15, 45, 75], o "cumsum()" gera um array que faz a soma acumulativa, gerando [15 60 135]
 
