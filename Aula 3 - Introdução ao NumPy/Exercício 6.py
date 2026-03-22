028afb6e63714a4b5f0c5254481a00e4c5102770
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-22 11:34:32 -0300

    Update Exercício 6.py

--- Aula 3 - Introdução ao NumPy/Exercício 6.py
+++ Aula 3 - Introdução ao NumPy/Exercício 6.py
@@ -7,5 +7,5 @@ import numpy as np
 pontuações = [[6.5, 7.8, 5.4], [9.5, 8.8, 9.1], [4.5, 7.3, 5.9]]
 
 arr = np.array(pontuações)
-arr_soma = np.sum(arr, axis=0)
-print(arr_soma.max())
+arr_soma = np.sum(arr, axis=0) #usando o "axis = 0" na soma, retorna 1 array que faz a soma pelas colunas, nesse caso, gera [20.5 23.9 20.4]
+print(arr_soma.max()) #aqui pegamos o maior desse array novo.
