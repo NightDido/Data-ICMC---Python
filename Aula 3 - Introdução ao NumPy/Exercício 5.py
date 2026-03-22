4af70362d01202cb48f6ae44c104017e9e9e952f
NightDido <Rodrigo.sartor.pinheiro@gmail.com>
2026-03-22 11:21:26 -0300

    Update Exercício 5.py

--- Aula 3 - Introdução ao NumPy/Exercício 5.py
+++ Aula 3 - Introdução ao NumPy/Exercício 5.py
@@ -8,4 +8,4 @@ pesos = np.array ([3, 2, 1])
 def media_ponderada (valores, pesos):
     return np.sum(valores * pesos) / np.sum(pesos) #(valores * pesos) gera um array [3 4 5], o "np.sum()" soma esse array retornando um número. A mesma coisa acontece com o array "pesos"
 
-print(media_ponderada(valores,pesos)) #retorna o valor 2
+print(media_ponderada(valores,pesos)) #retorna o valor 2.0
